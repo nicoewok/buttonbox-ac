c6246707b7de8818c132f04867e56e64476bd7fa
NicoL <nicolang0416@gmail.com>
2024-02-15 14:39:30 +0100

    prepare code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,42 +1,92 @@
 #include <Joystick.h>
 
-//Buttons
-#define button1 4
-#define button2 9
-#define switcher 10
+//Defining row/column pins on your board
+#define ROW1 1
+#define ROW2 1
+#define ROW3 1
+#define COL1 1
+#define COL2 1
+#define COL3 1
+#define COL4 1
+#define SPIN 1
 
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
-  5, 0,                  // Button Count, Hat Switch Count
+  13, 0,                  // Button Count, Hat Switch Count
   false, false, false,     // X and Y, but no Z Axis
   false, false, false,   // No Rx, Ry, or Rz
   false, false,          // No rudder or throttle
   false, false, false);  // No accelerator, brake, or steering
 
 void setup() {
-  // Initialize Button Pins
-  pinMode(button1, INPUT_PULLUP);
+  //Initialize pins
+  pinMode(ROW1, INPUT_PULLUP);
+  pinMode(ROW2, INPUT_PULLUP);
+  pinMode(ROW3, INPUT_PULLUP);
+  pinMode(COL1, INPUT_PULLUP);
+  pinMode(COL2, INPUT_PULLUP);
+  pinMode(COL3, INPUT_PULLUP);
+  pinMode(COL4, INPUT_PULLUP);
+  pinMode(SPIN, INPUT_PULLUP);
   Joystick.begin(false);
 }
 
 
 void loop() {
-  //check if up
+  /*Buttons are in following order according to wiring diagramm:
+    u1 | d1 | u2 | d2 | u3 | d3 | u4 | d4 | b1 | b2 | u5 | d5 | b3
+     0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 | 11 | 12
+  */
+  //Row-wise checking
+  if (digitalRead(ROW1) == LOW) {
+    if (digitalRead(COL1) == LOW) {
+      Joystick.pressButton(0);
+    } else if (digitalRead(COL2) == LOW) {
+      Joystick.pressButton(2);
+    } else if (digitalRead(COL3) == LOW) {
+      Joystick.pressButton(4);
+    } else if (digitalRead(COL4) == LOW) {
+      Joystick.pressButton(6);
+    }
+  } else if (digitalRead(ROW2) == LOW) {
+    if (digitalRead(COL1) == LOW) {
+      Joystick.pressButton(1);
+    } else if (digitalRead(COL2) == LOW) {
+      Joystick.pressButton(3);
+    } else if (digitalRead(COL3) == LOW) {
+      Joystick.pressButton(5);
+    } else if (digitalRead(COL4) == LOW) {
+      Joystick.pressButton(7);
+    }
+  } else if (digitalRead(ROW3) == LOW) {
+    if (digitalRead(COL1) == LOW) {
+      Joystick.pressButton(9);
+    } else if (digitalRead(COL2) == LOW) {
+      //not possible
+    } else if (digitalRead(COL3) == LOW) {
+      Joystick.pressButton(10);
+    } else if (digitalRead(COL4) == LOW) {
+      Joystick.pressButton(12);
+    }
+  }
+  //checking special pin
+  else if (digitalRead(SPIN) == LOW) {
+    if (digitalRead(ROW2) == LOW) {
+      Joystick.pressButton(8);
+    } else if (digitalRead(COL3) == LOW) {
+      Joystick.pressButton(11);
+    }
+  }
 
-  Joystick.sendState();//send state 1st before we clear the button states
+  //send state then clear buttons again
+  Joystick.sendState();
   ClearButtons();
-  
-
-  Serial.print("button1 state:");
-  Serial.println(digitalRead(button1));
 
   delay(1000);
 }
 
-void ClearButtons()
-{
-  Joystick.releaseButton(0);
-  Joystick.releaseButton(1);
-  Joystick.releaseButton(2);
-  Joystick.releaseButton(3);
+void ClearButtons() {
+  for (int i = 0; i < 13; ++i) {
+    Joystick.releaseButton(i);
+  }
 }
